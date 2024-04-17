41f1fd5704308241e6d7f7f4ef9ed25e79dc7d6c
KarenMilene006 <118762191+KarenMilene006@users.noreply.github.com>
2024-04-16 21:59:46 -0300

    Update Main

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Header } from '../../components/Header'
 import { Main } from './styles'
+import Bigmac from '../../assets/bigmac.png'
+import Prettymac from '../../assets/bigmac(1).png'
+import Sorvete from '../../assets/sorvete.png'
+import Batata from '../../assets/batata.png'
 
 
 export const Home = () => {
@@ -9,9 +13,32 @@ export const Home = () => {
     <Header/>
 
     <Main>
+
+      
 <section className='principal'>
-   <h1>ola eu sou a main</h1>
+
+
+<div className='fastfoodtext'>
+
+<img src={Bigmac} alt='bigmac'/>
+
+   <h1>BATEU AQUELA #FOME DE MÉQUI?</h1>
+   </div>
+
+
+
+   <div className='fastfoodimg'>
+    <img src={Prettymac} alt='bigmac'/>
+    <img src={Sorvete} alt='bigmac'/>
+    <img src={Batata} alt='bigmac'/>
+   </div>
    </section>
+
+
+
+
+
+
 
    <section className='segundaria'>
    <h1>ola eu sou a main</h1>
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -8,6 +8,8 @@ width: 100%;
 height:100vh;
 }
 
+
+
 .segundaria{
     background: #FEB706;
 }
